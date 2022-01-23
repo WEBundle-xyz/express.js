8a91e2ea279b2db947ccbe02014a28a3a1dfa118
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2022-01-23 08:58:14 -0500

    Parsing incomig requests

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [2 - Added middleware ](../../tree/b7ab5a23cd3c41af192c14dda6693518c0c08de0/)
 
-[3 - Empty ](../../tree//)
+[3 - Handling different routes ](../../tree/c9cba7956db862549076949e353b5beb960b4876/)
 
 [4 - Empty ](../../tree//)
 
--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/users', (req, res, next) => {
-	console.log('In another middleware!');
-	res.send('<h1>The users Page</h1>');
+app.use('/add-product', (req, res, next) => {
+	res.send(
+		'<html><body><form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></form>'
+	);
+});
+
+app.use('/product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-	console.log('In another middleware!');
 	res.send('<h1>The Main Page</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
